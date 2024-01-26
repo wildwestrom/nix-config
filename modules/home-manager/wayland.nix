e0eec5abeed8894ca77ee64a21fc91407b32f76d
Christian Westrom <c.westrom@westrom.xyz>
2024-01-26 19:49:19 +0900

    add wlsunset and set the location to Seoul

--- modules/home-manager/wayland.nix
+++ modules/home-manager/wayland.nix
@@ -14,5 +14,11 @@
       enable = true;
       defaultTimeout = 1000 * 5;
     };
+    wlsunset = {
+      enable = true;
+      temperature.night = 3000;
+      latitude = "37.5";
+      longitude = "127.0";
+    };
   };
 }
