8aa3c5c7f3625f561fb28f79b9d58aba2dbd5094
Christian Westrom <c.westrom@westrom.xyz>
2024-02-03 20:33:50 +0900

    comment out gtkgreet in favor of auto login

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -86,28 +86,28 @@ in {
 
   # Enable automatic login for the user.
   services.getty.autologinUser = "main";
-  environment.etc."greetd/environments".text = ''
-    sway
-    fish
-  '';
-  services.greetd = let
-    swayConfig = pkgs.writeText "greetd-sway-config" ''
-      # `-l` activates layer-shell mode. Notice that `swaymsg exit` will run after gtkgreet.
-      exec "${pkgs.greetd.gtkgreet}/bin/gtkgreet -l; swaymsg exit"
-      bindsym Mod4+shift+e exec swaynag \
-        -t warning \
-        -m 'What do you want to do?' \
-        -b 'Poweroff' 'systemctl poweroff' \
-        -b 'Reboot' 'systemctl reboot'
-    '';
-  in {
-    enable = true;
-    settings = {
-      default_session = {
-        command = "${pkgs.sway}/bin/sway --config ${swayConfig}";
-      };
-    };
-  };
+  # environment.etc."greetd/environments".text = ''
+  #   sway
+  #   fish
+  # '';
+  # services.greetd = let
+  #   swayConfig = pkgs.writeText "greetd-sway-config" ''
+  #     # `-l` activates layer-shell mode. Notice that `swaymsg exit` will run after gtkgreet.
+  #     exec "${pkgs.greetd.gtkgreet}/bin/gtkgreet -l; swaymsg exit"
+  #     bindsym Mod4+shift+e exec swaynag \
+  #       -t warning \
+  #       -m 'What do you want to do?' \
+  #       -b 'Poweroff' 'systemctl poweroff' \
+  #       -b 'Reboot' 'systemctl reboot'
+  #   '';
+  # in {
+  #   enable = true;
+  #   settings = {
+  #     default_session = {
+  #       command = "${pkgs.sway}/bin/sway --config ${swayConfig}";
+  #     };
+  #   };
+  # };
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
