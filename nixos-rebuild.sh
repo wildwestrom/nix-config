bb34d06d164b38b5904938681b380ca5253e59ed
Christian Westrom <c.westrom@westrom.xyz>
2024-02-26 02:02:42 +0900

    add a rebuild script

---
+++ nixos-rebuild.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# A rebuild script that commits on a successful build
+set -e
+
+pushd ~/nix-config/
+$EDITOR .
+alejandra . &>/dev/null
+git diff -U0 *.nix
+echo "NixOS Rebuilding..."
+sudo nixos-rebuild switch --flake '.#default' &>nixos-switch.log || (cat nixos-switch.log | grep --color error && false)
+current=$(nixos-rebuild list-generations | grep current)
+git commit -am "$current"
+popd
+notify-send -e "NixOS Rebuilt OK!" --icon=software-update-available
