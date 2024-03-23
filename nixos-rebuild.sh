d1202a249b5fa1a9e4305447c9110f2c0de74358
Christian Westrom <c.westrom@westrom.xyz>
2024-03-23 12:32:06 +0900

    exclude the lockfile in diffs

--- nixos-rebuild.sh
+++ nixos-rebuild.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 # A rebuild script that commits on a successful build
 set -e
+shopt -s extglob
 
 pushd ~/nix-config/
 $EDITOR .
 alejandra . &>/dev/null
-git diff -U0 **/*
+GLOBIGNORE="*.lock"
+git diff -U0 * **/*
 echo "NixOS Rebuilding..."
 sudo nixos-rebuild switch --flake '.#default' --option eval-cache false --show-trace &>nixos-switch.log || (cat nixos-switch.log | grep --color error && false)
 current=$(nixos-rebuild list-generations | grep current)
