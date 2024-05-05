645753374ad5596201f2680a3282ffdc9ed58221
Christian Westrom <c.westrom@westrom.xyz>
2024-05-05 16:51:14 +0900

    264 current  2024-05-05 16:47:17  24.05.20240309.3030f18   6.6.21                          *

--- flake.nix
+++ flake.nix
@@ -17,21 +17,27 @@
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
     sway-git-overlay.url = "github:jalil-salame/sway-git-overlay";
+
+    nixpkgs-wayland = {
+      url = "github:nix-community/nixpkgs-wayland";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
+    nixpkgs-wayland,
     nixos-hardware,
     sway-git-overlay,
     ...
   } @ inputs: let
     system = "x86_64-linux";
-    pkgs = import nixpkgs {
+    pkgs = import nixpkgs-wayland {
       inherit system;
       overlays = [
         # sway-git-overlay.overlays.default # provides sway-git
-        sway-git-overlay.overlays.replace-sway # replaces sway with sway-git (anywhere you use pkgs.sway you'll get pkgs.sway-git instead)
+        # sway-git-overlay.overlays.replace-sway # replaces sway with sway-git (anywhere you use pkgs.sway you'll get pkgs.sway-git instead)
         # (final: prev: let
         #   zedpkgs = nixpkgs-zed.legacyPackages.x86_64-linux;
         # in {
