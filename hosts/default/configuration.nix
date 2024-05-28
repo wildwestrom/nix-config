334f36f472500312e5ff81c313d6b560e6182eda
Christian Westrom <c.westrom@westrom.xyz>
2024-05-29 01:50:54 +0900

    304 current  2024-05-29 01:50:39  24.05.20240309.3030f18   6.6.21                          *

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -171,6 +171,8 @@ in
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
+  services.mullvad-vpn.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
