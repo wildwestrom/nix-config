0f6e7a6e7e57c6e474854bdc750eda9f5e96b819
Christian Westrom <c.westrom@westrom.xyz>
2024-06-24 17:32:10 +0900

    337 current  2024-06-24 17:32:06  24.05.20240309.3030f18  6.6.21                          *

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -166,10 +166,18 @@ in
   # services.openssh.enable = true;
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  # 53317 = LocalSend
+  networking.firewall.allowPing = true;
+  networking.firewall.allowedTCPPorts = [
+    22
+    80
+    443
+    53317
+  ];
+  networking.firewall.allowedUDPPorts = [
+    53
+    53317
+  ];
 
   services.resolved.enable = true;
   services.mullvad-vpn = {
