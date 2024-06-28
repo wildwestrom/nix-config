6a332ffcd9042164c64051dd96efe08bc2465e02
Christian Westrom <c.westrom@westrom.xyz>
2024-06-28 12:11:33 +0900

    341 current  2024-06-28 12:11:29  24.05.20240309.3030f18  6.6.21                          *

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -71,7 +71,7 @@ in
     ];
     sessionVariables = {
       XCURSOR_THEME = cursor-theme;
-      GTK_THEME = "${system-theme}";
+      # GTK_THEME = "${system-theme}";
     };
   };
   systemd.user.services = {
@@ -161,8 +161,8 @@ in
   services.syncthing.enable = true;
   gtk = {
     enable = true;
-    theme.name = system-theme;
-    # theme.package = pkgs.gnome-themes-extra;
+    # theme.name = system-theme;
+    # # theme.package = pkgs.gnome-themes-extra;
     cursorTheme.name = cursor-theme;
     cursorTheme.size = 32; # TODO: Test different values
     iconTheme.name = icon-theme;
