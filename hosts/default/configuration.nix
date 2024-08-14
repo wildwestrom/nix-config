c7550fe98094faeb354f9c88f49337cc390de0a4
Christian Westrom <c.westrom@westrom.xyz>
2024-08-14 23:00:14 +0900

    402 current  2024-08-14 23:00:11  24.05.20240309.3030f18  6.7.9                           *

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -21,6 +21,7 @@ in
   ];
 
   boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelParams = [ "amdgpu.sg_display=0" ];
 
   # nix.nixPath = [ "nixos-cofnig=/home/${username}/${config-dir}" ];
 
