fb73e6062124696cf15942b9d6ae322b1a2a3ce4
Christian Westrom <c.westrom@westrom.xyz>
2024-08-31 18:02:49 +0900

    417 current  2024-08-31 18:02:45  24.05.20240309.3030f18  6.7.9                           *

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -477,10 +477,6 @@ in
     text = ''
       [build]
       rustc-wrapper = "${pkgs.sccache}/bin/sccache"
-
-      [target.x86_64-unknown-linux-gnu]
-      linker = "clang"
-      rustflags = ["-C", "link-arg=-fuse-ld=${pkgs.mold}/bin/mold"]
     '';
   };
 }
