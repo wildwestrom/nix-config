a8736cfd6333d0c35e92611a5c258bda70646334
Christian Westrom <c.westrom@westrom.xyz>
2024-10-12 20:41:51 +0900

    458 current  2024-10-12 20:37:10  24.05.20240309.3030f18  6.7.9                           *

--- nixos-rebuild.sh
+++ nixos-rebuild.sh
@@ -9,6 +9,7 @@ nixfmt . &>/dev/null
 GLOBIGNORE="*.lock"
 git diff -U0 * **/*
 echo "NixOS Rebuilding..."
+sudo nix-channel --update
 sudo nixos-rebuild switch --upgrade --flake '.#default' --option eval-cache false --show-trace &>nixos-switch.log || (cat nixos-switch.log | grep --color error && false)
 current=$(nixos-rebuild list-generations | grep current)
 git commit -am "$current"
