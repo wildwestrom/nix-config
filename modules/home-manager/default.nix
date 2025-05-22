e478d0d5e0a78b9ef55a8c9334c9a0420a5f125f
Christian Westrom <c.westrom@westrom.xyz>
2025-05-22 12:11:30 +0900

    87 current  2025-05-20 20:23:31  25.11.20250518.292fa7d  6.14.6                          *

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -404,6 +404,33 @@ in
         tab_spaces = 4
       '';
     };
+    "fcitx5" = {
+      text = ''
+        [Groups/0]
+        Name=Default
+        Default Layout=us
+        DefaultIM=hangul
+
+        [Groups/0/Items/0]
+        Name=keyboard-us
+        Layout=
+
+        [Groups/0/Items/1]
+        Name=hangul
+        Layout=
+
+        [Groups/0/Items/2]
+        Name=mozc
+        Layout=
+
+        [Groups/0/Items/3]
+        Name=rime
+        Layout=
+
+        [GroupOrder]
+        0=Default
+      '';
+    };
   };
   home.file = {
     ".cargo" = {
