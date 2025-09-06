394855669c36ab4403fca581d8cb341d448ae638
Christian Westrom <c.westrom@westrom.xyz>
2025-09-06 23:38:08 +0900

    227

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -29,6 +29,7 @@ in
   ];
 
   boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelModules = [ "i2c-dev" ];
   boot.extraModulePackages = with config.boot.kernelPackages; [ ddcci-driver ];
   boot.kernelParams = [
     "amdgpu.sg_display=0"
