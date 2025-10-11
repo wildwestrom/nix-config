69150b41a1d86286d531b470fb3bf96438e444fd
Christian Westrom <c.westrom@westrom.xyz>
2025-10-12 01:46:17 +0900

    258

--- modules/home-manager/gaming.nix
+++ modules/home-manager/gaming.nix
@@ -1,12 +1,29 @@
 {
   pkgs,
+  unstable,
+  # unstable-unfree,
   ...
 }:
 {
 
   home = {
     packages = with pkgs; [
-      retroarch
+      (unstable.retroarch.withCores (
+        cores: with cores; [
+          dosbox-pure
+          bsnes
+          nestopia
+          mgba
+          meteor
+          fceumm
+          # pcsx-rearmed
+          # pcsx2
+          mame
+          # dolphin
+          parallel-n64
+          stella2014
+        ]
+      ))
       retroarch-assets
       retroarch-joypad-autoconfig
     ];
