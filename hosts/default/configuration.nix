d952ecc91c06d18b41c7784c352211e1d8193f82
Christian Westrom <c.westrom@westrom.xyz>
2025-12-07 17:39:09 +0900

    287

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -587,10 +587,14 @@ in
       "https://nix-community.cachix.org"
       "https://zed.cachix.org"
       "https://cache.garnix.io"
+      "https://devenv.cachix.org"
     ];
     trusted-public-keys = [
       "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "zed.cachix.org-1:/pHQ6dpMsAZk2DiP4WCL0p9YDNKWj2Q5FL20bNmw1cU="
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
     ];
   };
 
