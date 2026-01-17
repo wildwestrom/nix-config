4cbfcbce5cb1a38b2ca3ffe7c238295afd53b8bf
Christian Westrom <c.westrom@westrom.xyz>
2026-01-17 04:38:49 -0500

    310

--- hosts/default/hardware-configuration.nix
+++ hosts/default/hardware-configuration.nix
@@ -65,6 +65,8 @@
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
+  hardware.keyboard.qmk.enable = true;
+
   services.btrfs.autoScrub = {
     enable = true;
     interval = "weekly";
