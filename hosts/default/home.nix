1ef506e569e851daef93d1100c63680759be7470
Christian Westrom <c.westrom@westrom.xyz>
2026-03-22 15:30:32 +0900

    347

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -115,38 +115,38 @@
       templates = "${config.home.homeDirectory}/templates";
       videos = "${config.home.homeDirectory}/vids";
     };
-    # mimeApps = {
-    #   enable = false;
-    #   defaultApplications = {
-    #     "image/gif" = "imv-dir.desktop";
-    #     "image/jpeg" = "imv-dir.desktop";
-    #     "image/png" = "imv-dir.desktop";
-    #     "image/webp" = "imv-dir.desktop";
-    #     # "application/pdf" = "org.pwmt.zathura-pdf-mupdf.desktop";
-    #     "text/html" = "librewolf.desktop";
-    #     "text/xml" = "librewolf.desktop";
-    #     "application/rdf+xml" = "librewolf.desktop";
-    #     "application/rss+xml" = "librewolf.desktop";
-    #     "application/xhtml+xml" = "librewolf.desktop";
-    #     "application/xhtml_xml" = "librewolf.desktop";
-    #     "application/xml" = "librewolf.desktop";
-    #     "x-scheme-handler/http" = "librewolf.desktop";
-    #     "x-scheme-handler/https" = "librewolf.desktop";
-    #     "x-scheme-handler/ipfs" = "librewolf.desktop";
-    #     "x-scheme-handler/ipns" = "librewolf.desktop";
-    #     "x-scheme-handler/about" = "librewolf.desktop";
-    #     "x-scheme-handler/unknown" = "librewolf.desktop";
-    #     "x-scheme-handler/vscodium" = [
-    #       "codium-url-handler.desktop"
-    #       "codium.desktop"
-    #     ];
-    #     "x-scheme-handler/mailto" = "thunderbird.desktop";
-    #     "message/rfc822" = "thunderbird.desktop";
-    #     "x-scheme-handler/mid" = "thunderbird.desktop";
-    #     "font/otf" = "org.gnome.font-viewer.desktop";
-    #     "video/mkv" = "mpv.desktop";
-    #   };
-    # };
+    mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "image/gif" = "imv-dir.desktop";
+        "image/jpeg" = "imv-dir.desktop";
+        "image/png" = "imv-dir.desktop";
+        "image/webp" = "imv-dir.desktop";
+        "application/pdf" = "sioyek.desktop";
+        "text/html" = "librewolf.desktop";
+        "text/xml" = "librewolf.desktop";
+        "application/rdf+xml" = "librewolf.desktop";
+        "application/rss+xml" = "librewolf.desktop";
+        "application/xhtml+xml" = "librewolf.desktop";
+        "application/xhtml_xml" = "librewolf.desktop";
+        "application/xml" = "librewolf.desktop";
+        "x-scheme-handler/http" = "librewolf.desktop";
+        "x-scheme-handler/https" = "librewolf.desktop";
+        "x-scheme-handler/ipfs" = "librewolf.desktop";
+        "x-scheme-handler/ipns" = "librewolf.desktop";
+        "x-scheme-handler/about" = "librewolf.desktop";
+        "x-scheme-handler/unknown" = "librewolf.desktop";
+        "x-scheme-handler/vscodium" = [
+          "codium-url-handler.desktop"
+          "codium.desktop"
+        ];
+        "x-scheme-handler/mailto" = "thunderbird.desktop";
+        "message/rfc822" = "thunderbird.desktop";
+        "x-scheme-handler/mid" = "thunderbird.desktop";
+        "font/otf" = "org.gnome.font-viewer.desktop";
+        "video/mkv" = "mpv.desktop";
+      };
+    };
   };
 
   programs = {
