dbaa1470c7da71b16e262d191639e1632b54a6d4
Christian Westrom <c.westrom@westrom.xyz>
2026-06-15 13:17:36 +0900

    431

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -368,6 +368,7 @@ in
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "25.05"; # Did you read the comment?
 
+  fonts.enableFontDir = true;
   fonts.packages = with pkgs; [
     sarasa-gothic
     noto-fonts
