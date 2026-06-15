245006f1020ac364764701be97a076ab2eed1c09
Christian Westrom <c.westrom@westrom.xyz>
2026-06-15 13:50:49 +0900

    432

--- modules/home-manager/niri.nix
+++ modules/home-manager/niri.nix
@@ -102,10 +102,11 @@ in
         // Widths cycled by Mod+R (switch-preset-column-width). Full width is
         // included so a single window can fill the screen.
         preset-column-widths {
-            proportion 1.0
             proportion 0.95
             proportion 0.5
+            proportion 0.3333333
             proportion 0.25
+            proportion 1.0
         }
         default-column-width { proportion 0.5; }
     }
